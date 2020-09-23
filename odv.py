f3c3bf7ad94684fb2cedd9786869d7de4785e194
Luca Manini <luca.manini@liceodavincitn.it>
2020-09-23 07:29:31 +0200

    Almost there.

--- odv.py
+++ odv.py
@@ -14,16 +14,30 @@ DAYS_SHIFT = {"lunedì":     0,
               "venerdì":   24,
               "sabato":    30}
 
-prof_dict = defaultdict(list)
+def make_lessons_list():
+    return [""] * 35
+prof_dict = defaultdict(make_lessons_list)
 with open(SRC, newline="", encoding="utf-16") as data:
     rows = list(csv.reader(data, delimiter=";"))[1:]
     for r in rows:
         o = Record(*r)
         prof = "%s - %s" % (o.DOC_COGN, o.DOC_NOME)
-        prof_dict[prof].append((
-            o.GIORNO, DAYS_SHIFT[o.GIORNO], o.ORA_INIZIO,
-            o.MAT_COD, o.CLASSE,
-            ))
+        prof = (o.DOC_COGN, o.DOC_NOME)
+        prof_dict[prof][DAYS_SHIFT[o.GIORNO]] = o.MAT_COD + " / " + o.CLASSE
+        aula = o.AULA
+        if "(" in aula:
+            aula = aula.split("(")[1]
+        if ")" in aula:
+            aula = aula.split(")")[0]
+        # aula = o.AULA.split("(")[1].split(")")[0]
+        prof_dict[prof][DAYS_SHIFT[o.GIORNO]] = aula + " / " + o.CLASSE
+            # o.GIORNO,
+            # DAYS_SHIFT[o.GIORNO],
+            # o.ORA_INIZIO,
     for prof, ss in sorted(prof_dict.items()):
-        if prof.startswith("Manini"):
-            print(prof, ss)
+        # if prof.startswith("Manini"):
+        #     print(prof, ss)
+        # ss.insert(0, prof)
+        data = list(prof) + ss
+        line = ",".join(data)
+        print(line)
