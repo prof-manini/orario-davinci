3f843843a728545609cea5f79a93f99af5f772b1
Luca Manini <luca.manini@liceodavincitn.it>
2020-10-04 16:20:00 +0200

    New file odv.py. Base module for school schedule related apps.

---
+++ odv.py
@@ -0,0 +1,295 @@
+# author: Luca Manini (luca.manini@liceodavincitn.it)
+
+# Base module for processing school lessons schedule data produced by
+# the EDT software and exported in CSV format (UTF-16 encoded).
+
+# The export file has one row for every "activity" by one theacher in
+# one class.  Each activity can span one or more hours! The columns
+# headers can be found in this source file in the definition of the
+# "Record" data type.
+
+import csv
+from collections import defaultdict, namedtuple
+import xlsxwriter
+
+# generic data structures and functions ----------------------------
+
+# This are the column headers from the first row of the export file.
+# I'll use them as names for the attributes of the "Record" type.  At
+# import time, I use the csv module to split the row into a list of
+# fields from which I create a Record object that I'll use a the base
+# for all the processing.
+
+Record = namedtuple("Record", """
+    NUMERO DURATA FREQUENZA MAT_COD MAT_NOME
+    DOC_COGN DOC_NOME CLASSE AULA PERIODICITA
+    SPECIFICA CO_DOC COEFF GIORNO ORA_INIZIO ALUNNI
+""")
+
+# Example of a (splitted) row's content:
+#
+# 344                             # numero
+# 2h00                            # durata
+# S                               # frequenza
+# INF                             # mat_cod
+# Informatica                     # mat_nome
+# Manini                          # doc_cogn
+# Luca                            # doc_nome
+# 2Psa                            # classe
+# <Lab...>Lab. Inf..1 (2° p 2.04) # aula
+# S                               # periodicita
+# ss                              # specifica
+# N                               # co_doc
+# 50/60                           # coeff
+# martedì                         # giorno
+# 07h50                           # ora_inizio
+# 0                               # alunni
+
+# In the odv-full-timetable program, I have to build a list of all the
+# lessons each professor teaches in the whole week, so it is useful to
+# know at which "offset" weeks data start.  In the raw data file, the
+# days of the week are written using the lowercase full italian names
+# (with accents!).
+
+DAYS_SHIFT = {"lunedì":     0,
+              "martedì":    8,
+              "mercoledì": 16,
+              "giovedì":   24,
+              "venerdì":   32,
+              "sabato":    40}
+
+DAYS_PER_WEEK = len(DAYS_SHIFT)
+
+# In the raw data file, the start time of each lesson is coded in the
+# format shown below, but it is useful to be able to easily convert it
+# in a sequence id.
+
+START_SHIFT = {
+    "07h50": 0,
+    "08h40": 1,
+    "09h30": 2,
+    "10h30": 3,
+    "11h20": 4,
+    "12h15": 5,
+    "13h10": 6,
+    "14h00": 7
+    }
+
+LESSONS_PER_DAY = len(START_SHIFT)
+LESSONS_PER_WEEK = LESSONS_PER_DAY * DAYS_PER_WEEK
+
+# The raw data file exported from the EDT software is a CSV file
+# encoded in UTF-16 (for some reason!). More often than not, a UTF-8
+# encoding is preferrable as is this more amendable to unix command
+# line tools like grep or less.  That means that this program should
+# be able to read both formats, "guessing" the correct one.
+
+def get_encoding(file):
+    for e in ["utf-8", "utf-16"]:
+        try:
+            f = open(file, encoding=e)
+            f.read()
+            f.close()
+            return e
+        except UnicodeDecodeError:
+            pass
+    raise ValueError(f"Unable to detect enconding for '{file}'")
+
+# code specific to full-timetable (tabellone) --------------------
+
+def make_lessons_list():
+    return [""] * LESSONS_PER_WEEK
+
+def format_room(room):
+
+    # Nel file di export, il dato AULA è piuttosto logorroico,
+    # qualcosa del tipo "Aula 3Hsa (2.50)" (quando va bene), me nel
+    # tabellone voglio qualcosa di più sintetico, ad esempio solo la
+    # classe.
+
+    if "(" in room:
+        room = room.split("(")[1]
+    if ")" in room:
+        room = room.split(")")[0]
+    room = room.replace("<Aule per gruppi>Aula Magna 4° piano", "A.M.")
+    return room
+
+def data_to_dict(raw_data):
+
+    # Questo è il dizionario che, per ciascun prof usato come chiave,
+    # contiene le relative ore di lezione.  Uso defaultdict così "al
+    # primo giro" ho già una lista di stringhe vuote (generata da
+    # make_lessons_list) della lunghezza corretta.
+
+    prof_dict = defaultdict(make_lessons_list)
+
+    enc = get_encoding(raw_data)
+    with open(raw_data, newline="", encoding=enc) as data:
+        rows = list(csv.reader(data, delimiter=";"))[1:]
+        for r in rows:
+
+            # Qui creo, a partire dalla "riga" letta da CSV, una
+            # struttura i cui (nomi degli) attributi sono definiti
+            # dalla classe Record e che ho lasciato uguali a quelli
+            # presenti nel file CSV.
+
+            o = Record(*r)
+
+            # I dati delle varie righe vengono raccolti in un
+            # dizionario in cui le chiavi sono i dati del docente, ad
+            # esempio la coppia cognome/nome.
+
+            prof_cod = (o.DOC_COGN, o.DOC_NOME)
+
+            # Dati sulla classe
+
+            room = format_room(o.AULA)
+
+            # Qui scelto cosa scrivere nelle celle del foglio. Posso
+            # mettere sola la classe, solo l'aula o quello che voglio.
+            # Qui ci sono alcuni esempi da (s)commentare.
+
+            # cell = room + " / " + o.CLASSE
+            cell = o.CLASSE
+            # cell = room
+
+            # Qui gestisco le ore "multiple" (consecutive). Nel file
+            # CSV, per ogni lezione c'è una sola riga, in cui si
+            # indica però anche la durata.  Quello che faccio qui è
+            # "duplicare il dato" per ciascuna ora di lezione,
+            # perdendo così il dato esplicito della durata.  Poi nella
+            # generazione dell'XLS mi preoccupa di fare il merge delle
+            # varie celle.
+
+            size = int(o.DURATA[0]) # 1h00, 2h00 etc
+            for i in range(size):
+                day_cod = (DAYS_SHIFT[o.GIORNO] +
+                           START_SHIFT[o.ORA_INIZIO] +
+                           i)
+                # if o.ORA_INIZIO == "13h10":
+                #     print(f"{day_cod=} {o.GIORNO=}")
+                prof_dict[prof_cod][day_cod] = cell
+
+    return prof_dict
+
+def write_prof_dict_csv(prof_dict, csv_out):
+
+    # Qui scrivo il risultato in un file CSV usando semplicemente un
+    # file di testo e relativo metodo "write".  Il modulo csv ha un
+    # metodo apposito che però non mi pare offra vantaggi
+    # interessanti.
+
+    with open(csv_out, "w") as output:
+        for prof_cod, ss in sorted(prof_dict.items()):
+            prof_surname, prof_firstname = prof_cod
+            prof_data = "%s %s." % (prof_surname, prof_firstname and
+                                    prof_firstname[0])
+            data = [prof_data] + ss
+            line = ",".join(data)
+            output.write(line + "\n")
+
+def write_prof_dict_xls(prof_dict, xsl_out):
+
+    # Nel dubbio, consultare:
+    # https://xlsxwriter.readthedocs.io/examples.html
+    # https://xlsxwriter.readthedocs.io/format.html#format
+
+    book = xlsxwriter.Workbook(xsl_out)
+
+    # Qui posso definire vari formati che poi utilizzo nelle chiamate
+    # a merge_range e a write (credo). Hanno un aspetto molto CSS, ma
+    # non so se la corrispondenza è completa.
+
+    title_format = book.add_format({
+        'align': 'center',
+        'bold': True,
+        'font_size': 20,
+        })
+    days_format = book.add_format({
+        'align': 'center',
+        'bold': True,
+        })
+    hours_format = book.add_format({
+        'align': 'center',
+        'bold': True,
+        })
+    merge_format = book.add_format({
+        'align': 'center',
+    })
+    cell_format = book.add_format({
+        'align': 'center',
+    })
+    prof_format = book.add_format({
+        'align': 'left',
+    })
+
+    sheet = book.add_worksheet()
+    sheet.set_default_row(20)
+    sheet.set_column(0, 0, 25, prof_format)
+    # sheet.set_column(1, 1, 25, prof_format)
+
+    # Scrittura della parte "fissa" di headers
+    row = 0
+    prof_off = 1         # numero di colonne usate per i dati del prof
+    if True:
+
+        # Titolo: centrato su tutta la larghezza (48 colonne)
+
+        sheet.set_row(row, 24)
+        sheet.merge_range(0,0,0,48, "Orario", title_format)
+        row += 1
+
+        # Giorni della settimana, presi da DAYS_SHIFT
+
+        sheet.set_row(row, 22)
+        days = [s.capitalize() for s in DAYS_SHIFT.keys()]
+        for i, day in enumerate(days):
+            range_start = prof_off + i * LESSONS_PER_DAY
+            range_end   = range_start + LESSONS_PER_DAY - 1
+            sheet.merge_range(row, range_start,
+                              row, range_end,
+                              day, days_format)
+        row += 1
+
+        # Ore del giorno, prese da START_SHIFT, in cui hanno un
+        # formato tipo 07h30 che converto in 7:30.
+
+        sheet.set_row(row, 20)
+        for d in range(DAYS_PER_WEEK):
+            for i, hour in enumerate(START_SHIFT.keys()):
+                if hour[0] == "0":
+                    hour = hour[1:]
+                hour = hour.replace("h", ":")
+                sheet.write(row, prof_off + i + d * LESSONS_PER_DAY, # ???
+                            hour, hours_format)
+        row += 1
+
+    # Scrittura delle righe relative alle ore di lezione. Tutto facile
+    # a parte raggruppare le "doppiette" o le "triplette". Per fare
+    # questo uso un "trucco", se una cella ha lo stesso contenuto
+    # della precedente faccio un merge; fortunatamente pare funzionare
+    # anche "ricorsivamente", per esempio facendo il merge di una
+    # terza ora con le precedenti due già raggruppate.
+
+    row_off = row
+    for row, (prof_cod, ss) in enumerate(sorted(prof_dict.items())):
+        row += row_off
+        prof_surname, prof_firstname = prof_cod
+        prof_data = "%s %s." % (prof_surname, prof_firstname and
+                                    prof_firstname[0])
+        data = [prof_data] + ss
+        old = None
+        for col, text in enumerate(data):
+            if col in [0,1]:
+                sheet.write(row, col, text, prof_format)
+            else:
+                if text and text == old:
+                    sheet.merge_range(row, col-1,
+                                      row, col,
+                                      text, merge_format)
+                    old = None
+                else:
+                    sheet.write(row, col, text, cell_format)
+                    old = text
+
+    book.close()
