7c9acc0c793ce467b0417a16ba3a9bc0457e179a
Luca Manini <luca.manini@liceodavincitn.it>
2021-04-08 19:50:33 +0200

    Close fix-calc-summaries-args.

--- simple.py
+++ simple.py
@@ -42,9 +42,16 @@ def fix_class(klass):
     ss = klass[1:].split("/")
     return [o + s for s in ss]
 
-def calc_summaries(rows, prof_set, klass_set, room_set, mat_dic):
+def calc_summaries(rows):
     """Collect selected fields from ROWS and fill given collections"""
     info("Calculating summaries")
+
+    cc_dic = defaultdict(set)
+    klass_set = set()
+    mat_dic = dict()
+    prof_set = set()
+    room_set = set()
+
     multi_class = 0
     for r in rows:
 
@@ -56,43 +63,33 @@ def calc_summaries(rows, prof_set, klass_set, room_set, mat_dic):
          _period, _spec, _co_doc, _coeff,
          day, start, _studs) = r
 
-        # klass = fix_class(klass)
-
         kk = fix_class(klass)
         if len(kk) > 1:
             multi_class += 1
-        for k in kk:
-            # cc[k].add((prof_surname, mat_cod))
 
+        for k in kk:
 
             prof_surname, prof_name = fix_prof(prof_surname, prof_name)
-            room = fix_room(room)
-
             prof_set.add((prof_surname, prof_name))
+
             klass_set.add(k)
+
             mat_dic[mat_cod] = mat_name
+
+            room = fix_room(room)
             room_set.add(room)
+
+            cc_dic[k.strip()].add((prof_surname, mat_cod))
+
     info(f"{multi_class} multi-class records found")
 
-def calc_cc(rows):
-    info("Calculating 'consigli di classe'")
-    cc = defaultdict(set)
-    for r in rows:
-
-        (num, duration, freq,
-         mat_cod, mat_name,
-         prof_surname, prof_name,
-         klass,
-         room,
-         _period, _spec, _co_doc, _coeff,
-         day, start, _studs) = r
-
-        prof_surname, prof_name = fix_prof(prof_surname, prof_name)
-
-        kk = fix_class(klass)
-        for k in kk:
-            cc[k.strip()].add((prof_surname, mat_cod))
-    return cc
+    return {
+        "Profs": prof_set,
+        "Classes": klass_set,
+        "Rooms": room_set,
+        "Subjects": mat_dic,
+        "CC": cc_dic,
+    }
 
 def string_collection(oo, name, limit=1000):
     """Show contents of OO and LEN of collections, prefix with NAME"""
@@ -105,10 +102,6 @@ def string_collection(oo, name, limit=1000):
     ss.insert(0, f"{name}: {len(oo)}")
     return "\n".join(ss)
 
-def show_collection(oo, name, limit=1000):
-    """Show contents of OO and LEN of collections, prefix with NAME"""
-    print(string_collection(oo, name, limit))
-
 def string_cc(cc):
     ss = list()
     for k,pp in sorted(cc.items()):
@@ -116,9 +109,6 @@ def string_cc(cc):
         for p,m in sorted(pp):
             ss.append(f"    {p:20s} {m:3s}")
     return "\n".join(ss)
-
-def show_cc(cc):
-    print(string_cc(cc))
 
 def save_cc(cc):
     f = f"simple-out/cc.txt"
@@ -128,53 +118,23 @@ def save_cc(cc):
         out.write(s)
 
 def save_summaries(rows):
-    prof_set = set()
-    klass_set = set()
-    room_set = set()
-    mat_dic = dict()
 
-    calc_summaries(rows, prof_set, klass_set, room_set, mat_dic)
-
-    cc = {
-        "Profs": prof_set,
-        "Classes": klass_set,
-        "Rooms": room_set,
-        "Subjects": mat_dic,
-    }
+    data = calc_summaries(rows)
 
     info("Saving summaries...")
-    for s,c in cc.items():
+    for s,c in data.items():
         f = f"simple-out/{s.lower()}.txt"
         info(f"...writing {len(c)} lines to '{f}'")
-        with open(f, "w") as out:
-            out.write(string_collection(c, s))
-
-def show_summaries(rows):
-    prof_set = set()
-    klass_set = set()
-    room_set = set()
-    mat_dic = dict()
-
-    calc_summaries(rows, prof_set, klass_set, room_set, mat_dic)
-
-    cc = {
-        "Profs": prof_set,
-        "Classes": klass_set,
-        "Rooms": room_set,
-        "Subjects": mat_dic,
-    }
-
-    for s,c in cc.items():
-        print("-" * 40)
-        show_collection(c, s, limit=10)
+        if s == "CC":
+            save_cc(c)
+        else:
+            with open(f, "w") as out:
+                out.write(string_collection(c, s))
 
 def main(input_csv_file):
+
     rows = file_to_rows(input_csv_file)
-
     save_summaries(rows)
-
-    cc = calc_cc(rows)
-    save_cc(cc)
 
 if __name__ == "__main__":
 
